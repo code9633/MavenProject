2fce611c683f503a3bc56c1f2a4aa890322fae38
Shashika.udara <shashikaudara6@gmail.com>
2024-12-23 13:25:54 +0530

    The third changes of the code

--- Single_module/src/test/java/com/example/TestGreeter.java
+++ Single_module/src/test/java/com/example/TestGreeter.java
@@ -48,4 +48,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+  @Test
+  public void greetShouldIncludeGreeting() {
+    String someone = "Hello world ";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }

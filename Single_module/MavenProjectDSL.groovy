2dc45cd0ab493d5696ad7e6cc4dc560372dd0524
Shashika.udara <shashikaudara6@gmail.com>
2024-12-28 01:13:27 +0530

    Error correcting

--- Single_module/MavenProjectDSL.groovy
+++ Single_module/MavenProjectDSL.groovy
@@ -2,7 +2,7 @@ job('First-Maven-Project-Via-DSL'){
     description ("First Maven job generated bu the DSL on ${new Date()}, the project  is a samll Maven Project hosted on github")
     //Source code management
     scm{
-        git("https://github.com/code9633/MavenProject.git", master)
+        git("https://github.com/code9633/MavenProject.git", 'master')
     }
     triggers{
         scm('* * * * *') // every minute triggered
